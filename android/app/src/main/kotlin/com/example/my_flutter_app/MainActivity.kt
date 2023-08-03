a391385400d2d153e707929111aecf1ba920ba58
Piyush <piyush.raj@23D-MACABL-0459.local>
2023-08-03 18:13:05 +0530

    chore: init and setup

---
+++ android/app/src/main/kotlin/com/example/my_flutter_app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.my_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingBackgroundService;
+
+class MainActivity: FlutterActivity() {
+//   @Override
+//   public void onCreate() {
+//     super.onCreate();
+//     FlutterFirebaseMessagingBackgroundService.setPluginRegistrant(this);
+//   }
+
+//   @Override
+//   public void registerWith(PluginRegistry registry) {
+//     GeneratedPluginRegistrant.registerWith(registry);
+//   }
+}
